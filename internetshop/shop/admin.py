a2dab36fcce07fcc7f37a3801b241e90745a3a18
Aleksandra <alexapaisheva@gmail.com>
2024-07-11 14:56:40 +0300

    Исправлены ошибочки 2.0

--- internetshop/shop/admin.py
+++ internetshop/shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from . import models
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+# Register your models here.
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Review, ReviewAdmin)
